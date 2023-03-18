45877a177506418d034554399e39e4fe32dd2cc7
TobiRino <oluwatobi.aderinokun222@gmail.com>
2023-03-18 23:00:47 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+    <!-- implememnt the router here -->
+    <router-view></router-view>
+  
+  </template>
+  
+  <script>
+  export default {
+    name: "App",
+  };
+  </script>
+  
+  <style>
+  *{
+    margin:0;
+    padding:0;
+    box-sizing: border-box;
+    /* background-color: blue; */
+    
+    background-size:cover;
+  }
+  
+  body {
+  /* background-image:url(assets/people.jpg);  */
+  background: linear-gradient( rgba(0,0,0,0.8), rgba(0,0,0,0.7)), url(assets/people.jpg);
+  background-size:cover;
+  background-position: center;
+  
+  }
+  .button
+  {
+    background-color: #4CAF50; /* Green */
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+  }
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: white;
+    text-align: center;
+    color: white;
+    margin-top: 60px;
+  
+  }
+  
+  
+  </style>
+  
---
+++ src/components/Home.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <h1 class="head1">Count People without losing track</h1>
+    </div>
+    
+  <!-- Routing the counter page -->
+    <router-link to="/counter" class="link">View Counter</router-link>
+  </template>
+  
+  <script></script>
+  
+  <style>
+  
+  .link{
+    padding: 20px;
+    background-color:rgb(47, 43, 117);
+    color:white;
+    border-radius:5px;
+    font-size: 20px;
+    border: none;
+    text-decoration: none;
+    margin-top: 20px;
+  }
+  
+  </style>
+  
+  
